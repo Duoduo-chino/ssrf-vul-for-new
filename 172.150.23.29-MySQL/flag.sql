7bb1d90eb75c7f9aa9d5b07a62cecd977795e991
Duoduo-chino <37808059+Duoduo-chino@users.noreply.github.com>
2024-09-19 09:08:42 +0800

    new
    new

---
+++ 172.150.23.29-MySQL/flag.sql
@@ -0,0 +1,32 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+drop database if exists flag;
+create database flag;
+use flag;
+
+-- ----------------------------
+-- Table structure for table
+-- ----------------------------
+DROP TABLE IF EXISTS `table`;
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `flag` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of table
+-- ----------------------------
+BEGIN;
+INSERT INTO `test` VALUES (1, 'flag_9_mysql_2{71a5d5e6b2b9a3da3dc0a85851d50316}');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+UPDATE mysql.user SET Password = '' WHERE User = 'root';
+
+UPDATE mysql.user SET Host = '%' WHERE Host = 'localhost' and User = 'root';
+
+FLUSH PRIVILEGES;
---
+++ 172.150.23.30-FastCGI/src-fpm/index.php
@@ -0,0 +1,3 @@
+<?php 
+echo "Hello FastCGI 未授权";
+?>
